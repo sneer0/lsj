c5b50c0ca2937d758293733e7e6fa5acebf8dc1b
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-01-21 19:07:12 +0800

    Update qtss.lua

--- 功能/qtss.lua
+++ 功能/qtss.lua
@@ -1,7 +1,6 @@
 --【其他搜索】
 
 items={}--复选框开始♠♠♠♠
-table.insert(items,"热梗搜索")
 table.insert(items,"哔站搜索")
 table.insert(items,"西瓜搜索")
 table.insert(items,"贴吧搜索")
@@ -13,9 +12,6 @@ table.insert(items,"抖音搜索")--待删
 AlertDialog.Builder(this)
 --.setTitle("其他搜索")--标题♥
 .setItems(items,{onClick=function(l,v) 
-    if items[v+1]=="热梗搜索" then
-      进入子页面("浏览器",{标题="热梗搜索",链接="https://jikipedia.com/search?phrase="..edit.text.."&category=definition"})
-    end
     if items[v+1]=="哔站搜索" then
       进入子页面("浏览器",{标题="哔站搜索",链接="https://search.bilibili.com/all?keyword="..edit.text})
     end
@@ -37,8 +33,7 @@ AlertDialog.Builder(this)
     if items[v+1]=="药监搜索" then
       进入子页面("浏览器",{标题="药监搜索",链接="https://www.nmpa.gov.cn/so/s?tab=all&qt="..edit.text})
     end
-    if items[v+1]=="抖音搜索" then
-      进入子页面("浏览器",{标题="抖音搜索",链接="https://www.douyin.com/search/"..edit.text})
-    end
   end})--结束
 .show()--复选框结束♠♠♠♠
+
+--进入子页面("浏览器",{标题="小鸡词典",链接="https://jikipedia.com/search?phrase="..edit.text.."&category=definition"})
