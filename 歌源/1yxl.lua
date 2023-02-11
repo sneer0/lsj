414a26aefeb0d1cb9ff0f4feb5a3cc757f292445
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-02-12 00:24:03 +0800

    Update 1yxl.lua

--- 歌源/1yxl.lua
+++ 歌源/1yxl.lua
@@ -35,7 +35,7 @@ AlertDialog.Builder(this)
       local txt="yzn"--●
       对话框()
       .设置标题("温馨提示")
-      .设置消息("启用智能线路后，每次打开软件系统都将智能选择最优线路。")
+      .设置消息("启用智能线路后，每次打开软件系统都将智能选择最优线路，缺点是加载时间会相对变慢。")
       .设置积极按钮("确定",function()
         io.open("/storage/emulated/0/Android/data/com.sneer.zgsjhhm/qhgy.txt","w+"):write(txt):close()
         activity.recreate()--重置界面
