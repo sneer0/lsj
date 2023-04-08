bda0e57d520dce7863e5cfc9c531b909ec1ae79f
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-04-08 21:04:38 +0800

    Update 1gxan.lua

--- 基础/1gxan.lua
+++ 基础/1gxan.lua
@@ -1,6 +1,20 @@
 --【基础:更新按钮♥♥♥】
 
-对话框()
+
+webView.clearCache(true)--清除缓存♥
+执行Shell("rm -rf /sdcard/"..this.packageName)
+执行Shell("rm -rf /storage/emulated/0/"..this.packageName)
+执行Shell("rm -rf /sdcard/Android/data/"..this.packageName)
+执行Shell("rm -rf /storage/emulated/0/Android/data/"..this.packageName)
+执行Shell("rm -rf /data/data/"..this.packageName.."/cache")
+执行Shell("rm -rf /data/data/"..this.packageName.."/code_cache")
+执行Shell("rm -rf /data/data/"..this.packageName.."/app_webview")
+执行Shell("rm -rf /data/data/"..this.packageName.."/app_textures")
+执行Shell("rm -rf /data/data/"..this.packageName.."/files/data")
+执行Shell("rm -rf /data/data/"..this.packageName.."/files/live_log")
+
+
+对话框()--备用更新♥
 .设置标题("备用更新")
 .设置消息("【浏览器更新】\n调用手机默认浏览器下载安装包，部分浏览器可能打不开下载页面。\n\n【软件内更新】\n在软件内调用系统下载器下载安装包，在通知栏点击安装包安装。")
 .设置积极按钮("浏览器更新",function()
@@ -14,7 +28,7 @@ end)
   进入子页面("浏览器",{标题="最新版本APP【请更新】",链接="https://sneer.lanzoui.com/b06s2z42d"})
 end)
 .显示()
---备用更新结束♥
+
 
 --安装apk开始♥
 task(120000,function()--延迟2分钟运行代码
