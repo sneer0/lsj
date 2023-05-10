91b327b6f3af18243261980a02b899e31f320115
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-05-10 11:51:56 +0800

    Update yzn.lua

--- 歌源/yzn.lua
+++ 歌源/yzn.lua
@@ -12,23 +12,6 @@ https://go.txca.ml/0:/lizhi/
 https://www.lizhi334.com/334
 http://150.158.140.38:8080
 http://www.soulseeker.cn/?page_id=238
-挂了http://sls-mp3-static-1301308367.cos-website.ap-guangzhou.myqcloud.com
+http://sls-mp3-static-1301308367.cos-website.ap-guangzhou.myqcloud.com
 挂了https://www.lizhinb.com/cd
---]]
-
-
---[[刷新开始
-task(5000,function()--延迟5秒
-  刷新网页()
-end)--刷新结束]]
-
---[[
-对话框()
-.设置标题("加载失败")
-.设置消息("若WIFI无法访问，请使用数据流量。")
-.设置积极按钮("确定",function()
-  刷新网页()
-end)
-.设置中立按钮("")
-.显示()
 --]]
