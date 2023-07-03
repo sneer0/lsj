56688fc7aa75dc4f58417e64ccbb5774d3c726ed
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-07-03 20:34:00 +0800

    Update xggw.lua

--- ad/xggw.lua
+++ ad/xggw.lua
@@ -310,7 +310,7 @@ drawerLayout.getChildAt(0).addView(loadlayout{
         layout_width="24dp",--按钮尺寸
         layout_height="24dp",--按钮尺寸
         layout_marginTop="0.9%w";--顶距",
-        ColorFilter="#FF888888";--图片蒙版
+        ColorFilter="#FF757575";--图片蒙版
         src="drawable/1_tz.png",--按钮路径◆
       };
       {
@@ -324,7 +324,7 @@ drawerLayout.getChildAt(0).addView(loadlayout{
         singleLine=true;--单行显示
         ellipsize="marquee",--显示     
         Selected=true;--显示
-        textColor='#FF888888';--文字颜色       
+        textColor='#FF757575';--文字颜色       
         text='留声机尝鲜版发布啦，点此可下载，欢迎大家体验~';--显示文字
         textSize='16dp';--文字大小
       };
