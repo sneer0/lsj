7078029653ac9b3ee11e4bf4a467c782df3a80b2
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-12-30 02:05:30 +0800

    更新 zygg.lua

--- ad/zygg.lua
+++ ad/zygg.lua
@@ -1,10 +1,10 @@
 {广告:
-【图一】https://txc.gtimg.com/data/292760/2023/1230/29357f16c5d975f7efe05e7f87cd10f6.jpeg【END】
-【标一】安吉白茶明前醇香新茶【END】
-【图二】https://txc.gtimg.com/data/292760/2023/1229/7a8611cca2a7cedd776191ba30579cf2.jpeg【END】
-【标二】安吉冬笋鲜脆爽口【END】
-【图三】https://txc.gtimg.com/data/292760/2023/1230/29357f16c5d975f7efe05e7f87cd10f6.jpeg【END】
+【图一】https://txc.gtimg.com/data/292760/2023/1229/7a8611cca2a7cedd776191ba30579cf2.jpeg【END】
+【标一】安吉冬笋鲜脆爽口【END】
+【图二】https://txc.gtimg.com/data/292760/2023/1230/29357f16c5d975f7efe05e7f87cd10f6.jpeg【END】
+【标二】安吉白茶明前醇香新茶【END】
+【图三】https://txc.gtimg.com/data/292760/2023/1229/7a8611cca2a7cedd776191ba30579cf2.jpeg【END】
 【标三】【END】
-【图四】https://txc.gtimg.com/data/292760/2023/1229/7a8611cca2a7cedd776191ba30579cf2.jpeg【END】
+【图四】https://txc.gtimg.com/data/292760/2023/1230/29357f16c5d975f7efe05e7f87cd10f6.jpeg【END】
 【标四】【END】
 :广告}
