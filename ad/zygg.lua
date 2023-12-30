3b7daee7de7b2597d210bf34485317505c747d92
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-12-30 10:48:36 +0800

    更新 zygg.lua

--- ad/zygg.lua
+++ ad/zygg.lua
@@ -1,33 +1,33 @@
 {广告:
 【图A】【END】
 【标A】【END】
-【链A】https://sneer0.github.io/lsj【END】
+【链A】https://shop221504625.taobao.com【END】
 
 【图B】【END】
 【标B】【END】
-【链B】【END】
+【链B】https://shop221504625.taobao.comhttps://shop221504625.taobao.com【END】
 
 【图C】【END】
 【标C】【END】
-【链C】【END】
+【链C】https://shop221504625.taobao.com【END】
 
 【图D】【END】
 【标D】【END】
-【链D】【END】
+【链D】https://shop221504625.taobao.com【END】
 
 【图E】【END】
 【标E】【END】
-【链E】【END】
+【链E】https://shop221504625.taobao.com【END】
 
 【图F】【END】
 【标F】【END】
-【链F】【END】
+【链F】https://shop221504625.taobao.com【END】
 
 【图G】【END】
 【标G】【END】
-【链G】【END】
+【链G】https://shop221504625.taobao.com【END】
 
 【图H】【END】
 【标H】【END】
-【链H】【END】
+【链H】https://shop221504625.taobao.com【END】
 :广告}
