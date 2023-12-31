9e117f02709a96c5f0c15f20d4b253884aa15cd7
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-12-31 21:12:42 +0800

    更新 xjsc.lua

--- gn/xjsc.lua
+++ gn/xjsc.lua
@@ -634,6 +634,4 @@ ss.onClick=function()--点击事件
   local txt="ynss"--页内搜索
   io.open("/storage/emulated/0/Android/data/com.sneer.lsj/hsp.txt","w+"):write(txt):close()
   进入子页面("调用",{标题="搜索下架说唱",链接="https://sneer0.github.io/lsj/wy/scss"})
-
-  弹出消息("点击右侧🔍按钮搜索")
 end--●搜索按钮●
